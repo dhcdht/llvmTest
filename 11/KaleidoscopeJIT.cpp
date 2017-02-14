b038e5be54c42620f4882927145d3547a00d96d6
dhcdht <dhcdht@qq.com>
2017-02-14 20:00:43 +0800

    添加 optimizeModule 函数

--- 11/KaleidoscopeJIT.cpp
+++ 11/KaleidoscopeJIT.cpp
@@ -3,13 +3,21 @@
 //
 
 #include "KaleidoscopeJIT.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
 
 
 KaleidoscopeJIT::KaleidoscopeJIT()
 : m_targetMachine(llvm::EngineBuilder().selectTarget()),
   m_dataLayout(m_targetMachine->createDataLayout()),
-  m_compileLayer(m_objectLayer, llvm::orc::SimpleCompiler(*m_targetMachine))
+  m_compileLayer(m_objectLayer, llvm::orc::SimpleCompiler(*m_targetMachine)),
+  m_optimizeLayer(m_compileLayer, [this](std::unique_ptr<llvm::Module> M) {
+      return this->optimizeModule(std::move(M));
+  }),
+  m_compileCallbackMgr(llvm::orc::createLocalCompileCallbackManager(m_targetMachine->getTargetTriple(), 0))
 {
+    auto indirectStubsMgrBuilder = llvm::orc::createLocalIndirectStubsManagerBuilder(m_targetMachine->getTargetTriple());
+    m_indirectStubsMgr = indirectStubsMgrBuilder();
     llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
 }
 
@@ -28,7 +36,7 @@ KaleidoscopeJIT::ModuleHandleT KaleidoscopeJIT::addModule(std::unique_ptr<llvm::
     // Lambda 2: Search for external symbols in the host process.
     auto Resolver = llvm::orc::createLambdaResolver(
             [&](const std::string &aName) {
-                if (auto sym = m_compileLayer.findSymbol(aName, false)) {
+                if (auto sym = m_optimizeLayer.findSymbol(aName, false)) {
                     return llvm::RuntimeDyld::SymbolInfo(sym.getAddress(), sym.getFlags());
                 }
 
@@ -48,16 +56,36 @@ KaleidoscopeJIT::ModuleHandleT KaleidoscopeJIT::addModule(std::unique_ptr<llvm::
 
     // Add the set to the JIT with the resolver we created above and a newly
     // created SectionMemoryManager.
-    return m_compileLayer.addModuleSet(std::move(modules), llvm::make_unique<llvm::SectionMemoryManager>(), std::move(Resolver));
+    return m_optimizeLayer.addModuleSet(std::move(modules), llvm::make_unique<llvm::SectionMemoryManager>(), std::move(Resolver));
 }
 
 void KaleidoscopeJIT::removeModule(KaleidoscopeJIT::ModuleHandleT aModule) {
-    m_compileLayer.removeModuleSet(aModule);
+    m_optimizeLayer.removeModuleSet(aModule);
 }
 
 llvm::orc::JITSymbol KaleidoscopeJIT::findSymbol(const std::string aName) {
     std::string mangledName;
     llvm::raw_string_ostream mangledNameStream(mangledName);
     llvm::Mangler::getNameWithPrefix(mangledNameStream, aName, m_dataLayout);
-    return m_compileLayer.findSymbol(mangledNameStream.str(), true);
+    return m_optimizeLayer.findSymbol(mangledNameStream.str(), true);
+}
+
+std::unique_ptr<llvm::Module> KaleidoscopeJIT::optimizeModule(std::unique_ptr<llvm::Module> module) {
+    // Create a function pass manager.
+    auto FPM = llvm::make_unique<llvm::legacy::FunctionPassManager>(module.get());
+
+    // Add some optimizations.
+    FPM->add(llvm::createInstructionCombiningPass());
+    FPM->add(llvm::createReassociatePass());
+    FPM->add(llvm::createGVNPass());
+    FPM->add(llvm::createCFGSimplificationPass());
+    FPM->doInitialization();
+
+    // Run the optimizations over all functions in the module being added to
+    // the JIT.
+    for (auto &F : *module) {
+        FPM->run(F);
+    }
+
+    return module;
 }
--- 11/KaleidoscopeJIT.h
+++ 11/KaleidoscopeJIT.h
@@ -10,8 +10,10 @@
 #include "llvm/ExecutionEngine/RTDyldMemoryManager.h"
 #include "llvm/ExecutionEngine/Orc/CompileUtils.h"
 #include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/IRTransformLayer.h"
 #include "llvm/ExecutionEngine/Orc/LambdaResolver.h"
 #include "llvm/ExecutionEngine/Orc/ObjectLinkingLayer.h"
+#include "llvm/ExecutionEngine/Orc/CompileOnDemandLayer.h"
 #include "llvm/IR/Mangler.h"
 #include "llvm/Support/DynamicLibrary.h"
 
@@ -23,8 +25,16 @@ private:
     llvm::orc::ObjectLinkingLayer<> m_objectLayer;
     llvm::orc::IRCompileLayer<decltype(m_objectLayer)> m_compileLayer;
 
+    typedef std::function<std::unique_ptr<llvm::Module>(std::unique_ptr<llvm::Module>)>
+            m_optimizeFunction;
+
+    llvm::orc::IRTransformLayer<decltype(m_compileLayer), m_optimizeFunction> m_optimizeLayer;
+
+    std::unique_ptr<llvm::orc::JITCompileCallbackManager> m_compileCallbackMgr;
+    std::unique_ptr<llvm::orc::IndirectStubsManager> m_indirectStubsMgr;
+
 public:
-    typedef decltype(m_compileLayer)::ModuleSetHandleT ModuleHandleT;
+    typedef decltype(m_optimizeLayer)::ModuleSetHandleT ModuleHandleT;
     KaleidoscopeJIT();
     ~KaleidoscopeJIT();
 
@@ -34,6 +44,8 @@ public:
     void removeModule(KaleidoscopeJIT::ModuleHandleT aModule);
 
     llvm::orc::JITSymbol findSymbol(const std::string aName);
+
+    std::unique_ptr<llvm::Module> optimizeModule(std::unique_ptr<llvm::Module> module);
 };
 
 
